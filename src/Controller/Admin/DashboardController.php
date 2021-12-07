895c07e5d48788afc0e4aadcbd1a9b7ddad4e501
Lamara2000 <mouzni.lamara@gmail.com>
2021-12-07 02:05:33 +0100

    a

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Realisation;
 use App\Entity\Fichier;
+use App\Entity\Message;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -32,5 +33,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Réalisations', 'fas fa-list', Realisation::class);
         yield MenuItem::linkToCrud('Fichiers', 'fas fa-list', Fichier::class);
+        yield MenuItem::linkToCrud('Messages', 'fas fa-list', Message::class);
     }
 }
